e2d16f1543f81b93cd3ada45db9d9a9d2f6b1f60
halfdane <HalfDane@gmx.net>
2016-03-01 14:14:17 +0100

    async loading of counter - it's just too slow.

---
+++ static/js/counter.js
@@ -0,0 +1,7 @@
+$(function() {
+  window.setTimeout(function() {
+    var statCounter = $('.stat-counter');
+    statCounter.append('<img src=' + statCounter.attr('data-src') + '>');
+  }, 100);
+
+});
